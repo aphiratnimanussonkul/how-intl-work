fb10c9448c56c34401815605cff952064b314886
Aphirat Nimanussonkul <Tata@odds.team>
2021-08-07 21:52:28 +0700

    add example datetime format

---
+++ date.time.format.ts
@@ -0,0 +1,120 @@
+const currentDate = new Date();
+
+console.log(new Intl.DateTimeFormat().formatToParts(currentDate));
+// [
+//     { type: 'month', value: '8' },
+//     { type: 'literal', value: '/' },
+//     { type: 'day', value: '7' },
+//     { type: 'literal', value: '/' },
+//     { type: 'year', value: '2021' }
+// ]
+
+console.log(new Intl.DateTimeFormat().format(currentDate));
+// 8/7/2021
+
+console.log(
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  }).format(currentDate)
+);
+// Aug 7, 2021
+
+console.log(
+  new Intl.DateTimeFormat("en", {
+    day: "2-digit",
+    month: "long",
+    year: "2-digit",
+  }).format(currentDate)
+);
+// August 07, 21
+
+console.log(new Intl.DateTimeFormat("th").format(currentDate));
+// 7/8/2564
+
+console.log(
+  new Intl.DateTimeFormat("th", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(currentDate)
+);
+// 7 สิงหาคม 2564
+
+console.log(
+  new Intl.DateTimeFormat("th", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "2-digit",
+  }).format(currentDate)
+);
+// 07/08/64
+
+console.log(
+  new Intl.DateTimeFormat("th", {
+    day: "numeric",
+    month: "narrow",
+    year: "numeric",
+  }).format(currentDate)
+);
+// 7 ส.ค. 2564
+
+console.log(
+  new Intl.DateTimeFormat("th", {
+    dateStyle: "full",
+  }).format(currentDate)
+);
+// วันเสาร์ที่ 7 สิงหาคม พ.ศ. 2564
+
+console.log(
+  new Intl.DateTimeFormat("th", {
+    dateStyle: "long",
+  }).format(currentDate)
+);
+// 7 สิงหาคม 2564
+
+console.log(
+  new Intl.DateTimeFormat("th", {
+    timeZoneName: "long",
+  }).format(currentDate)
+);
+// 7/8/2564 เวลาอินโดจีน
+
+console.log(
+  new Intl.DateTimeFormat("en-US", {
+    timeStyle: "long",
+  }).formatToParts(currentDate)
+);
+// [
+//   { type: "hour", value: "6" },
+//   { type: "literal", value: ":" },
+//   { type: "minute", value: "20" },
+//   { type: "literal", value: ":" },
+//   { type: "second", value: "59" },
+//   { type: "literal", value: " " },
+//   { type: "dayPeriod", value: "PM" },
+//   { type: "literal", value: " " },
+//   { type: "timeZoneName", value: "GMT+7" },
+// ];
+
+// dd-mm-yyyy
+const dateParts = new Intl.DateTimeFormat("en", {
+  day: "2-digit",
+  month: "2-digit",
+  year: "numeric",
+}).formatToParts();
+
+console.log(
+  `${dateParts[2].value}-${dateParts[0].value}-${dateParts[4].value}`
+);
+
+// dd-MMM-yyyy
+const dateParts2 = new Intl.DateTimeFormat("en", {
+  day: "2-digit",
+  month: "short",
+  year: "numeric",
+}).formatToParts();
+console.log(
+  `${dateParts2[2].value}-${dateParts2[0].value}-${dateParts2[4].value}`
+);
--- number.format.ts
+++ number.format.ts
@@ -10,76 +10,153 @@
 // maximumSignificantDigits?: number;
 // useGrouping: boolean;
 
-console.log(Intl.NumberFormat("en").format(9999))
+console.log(Intl.NumberFormat("en").format(9999));
 // 9,999
 
-console.log(Intl.NumberFormat("en", {useGrouping: false}).format(9999))
+console.log(Intl.NumberFormat("en", { useGrouping: false }).format(9999));
 // 9999
 
-console.log(Intl.NumberFormat("en", {minimumFractionDigits: 2}).format(9999))
+console.log(Intl.NumberFormat("en", { minimumFractionDigits: 2 }).format(9999));
 // 9,999.00
 
-console.log(Intl.NumberFormat("en", {maximumFractionDigits: 2}).format(9999))
+console.log(Intl.NumberFormat("en", { maximumFractionDigits: 2 }).format(9999));
 // 9,999
 
-console.log(Intl.NumberFormat("en", {minimumFractionDigits: 2}).format(9999.555))
+console.log(
+  Intl.NumberFormat("en", { minimumFractionDigits: 2 }).format(9999.555)
+);
 // 9,999.555
 
-console.log(Intl.NumberFormat("en", {maximumFractionDigits: 2}).format(9999.555))
+console.log(
+  Intl.NumberFormat("en", { maximumFractionDigits: 2 }).format(9999.555)
+);
 // 9,999.556
 
-console.log(Intl.NumberFormat("en", {minimumSignificantDigits: 2}).format(9999.555))
+console.log(
+  Intl.NumberFormat("en", { minimumSignificantDigits: 2 }).format(9999.555)
+);
 // 9,999.555
 
-console.log(Intl.NumberFormat("en", {maximumSignificantDigits: 2}).format(9999.555))
+console.log(
+  Intl.NumberFormat("en", { maximumSignificantDigits: 2 }).format(9999.555)
+);
 // 10,000
 
 //Currentcy System
 
-console.log(Intl.NumberFormat("en", {currencyDisplay: "code", currency: "USD" ,style:"currency"}).format(9999))
+console.log(
+  Intl.NumberFormat("en", {
+    currencyDisplay: "code",
+    currency: "USD",
+    style: "currency",
+  }).format(9999)
+);
 // USD 9,999.00
 
-console.log(Intl.NumberFormat("en", {currencyDisplay: "symbol", currency: "USD" ,style:"currency"}).format(9999))
+console.log(
+  Intl.NumberFormat("en", {
+    currencyDisplay: "symbol",
+    currency: "USD",
+    style: "currency",
+  }).format(9999)
+);
 // $9,999.00
 
-console.log(Intl.NumberFormat("en", {currencyDisplay: "narrowSymbol", currency: "USD" ,style:"currency"}).format(9999))
+console.log(
+  Intl.NumberFormat("en", {
+    currencyDisplay: "narrowSymbol",
+    currency: "USD",
+    style: "currency",
+  }).format(9999)
+);
 // $9,999.00
 
-console.log(Intl.NumberFormat("en", {currencyDisplay: "name", currency: "THB" ,style:"currency"}).format(9999))
+console.log(
+  Intl.NumberFormat("en", {
+    currencyDisplay: "name",
+    currency: "THB",
+    style: "currency",
+  }).format(9999)
+);
 // 9,999.00 US dollars
 
-console.log(Intl.NumberFormat("th", {currencyDisplay: "code", currency: "THB" ,style:"currency"}).format(9999))
+console.log(
+  Intl.NumberFormat("th", {
+    currencyDisplay: "code",
+    currency: "THB",
+    style: "currency",
+  }).format(9999)
+);
 // THB 9,999.00
 
-console.log(Intl.NumberFormat("th", {currencyDisplay: "symbol", currency: "THB" ,style:"currency"}).format(9999))
+console.log(
+  Intl.NumberFormat("th", {
+    currencyDisplay: "symbol",
+    currency: "THB",
+    style: "currency",
+  }).format(9999)
+);
 // ฺฺ฿9,999.00
 
-console.log(Intl.NumberFormat("th", {currencyDisplay: "narrowSymbol", currency: "THB" ,style:"currency"}).format(9999))
+console.log(
+  Intl.NumberFormat("th", {
+    currencyDisplay: "narrowSymbol",
+    currency: "THB",
+    style: "currency",
+  }).format(9999)
+);
 // ฿9,999.00
 
-console.log(Intl.NumberFormat("th", {currencyDisplay: "name", currency: "THB" ,style:"currency"}).format(9999))
+console.log(
+  Intl.NumberFormat("th", {
+    currencyDisplay: "name",
+    currency: "THB",
+    style: "currency",
+  }).format(9999)
+);
 // 9,999.00 บาทไทย
 
 // Unit system
-console.log(Intl.NumberFormat("en", {unit: "kilometer-per-hour", style: "unit"}).format(120))
+console.log(
+  Intl.NumberFormat("en", { unit: "kilometer-per-hour", style: "unit" }).format(
+    120
+  )
+);
 // 120 km/h
 
-console.log(Intl.NumberFormat("en", {unit: "liter", style: "unit", unitDisplay: "long"}).format(120))
+console.log(
+  Intl.NumberFormat("en", {
+    unit: "liter",
+    style: "unit",
+    unitDisplay: "long",
+  }).format(120)
+);
 // 120 liters
 
-console.log(Intl.NumberFormat("en", {unit: "liter", style: "unit", unitDisplay: "narrow"}).format(120))
+console.log(
+  Intl.NumberFormat("en", {
+    unit: "liter",
+    style: "unit",
+    unitDisplay: "narrow",
+  }).format(120)
+);
 // 120L
 
-// Percent 
-console.log(Intl.NumberFormat("en", { style: "percent"}).format(1))
+// Percent
+console.log(Intl.NumberFormat("en", { style: "percent" }).format(1));
 // 100%
 
-console.log(Intl.NumberFormat("en", { style: "percent"}).format(0.483))
+console.log(Intl.NumberFormat("en", { style: "percent" }).format(0.483));
 // 48%
 
-console.log(Intl.NumberFormat("en", { style: "percent", maximumFractionDigits: 3}).format(0.483))
+console.log(
+  Intl.NumberFormat("en", {
+    style: "percent",
+    maximumFractionDigits: 3,
+  }).format(0.483)
+);
 // 48.3%
 
 //Decimal
-console.log(Intl.NumberFormat("en", { style: "decimal" }).format(9999.99))
+console.log(Intl.NumberFormat("en", { style: "decimal" }).format(9999.99));
 // 9,999.99
